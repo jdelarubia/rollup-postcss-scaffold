ee9dcace80d8dacdae9a3d1d43941c13662d713b
jdelarubia <jdelarubia@gmail.com>
2022-01-07 19:37:10 +0100

    add postcss-import to pipeline

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
-
 module.exports = {
   plugins: {
+    "postcss-import": true /* Enable imports in CSS files */,
     "postcss-nesting": true /* Nesting CSS */,
     autoprefixer: true /* CSS Vendor prefixes */,
     "postcss-purgecss": {
